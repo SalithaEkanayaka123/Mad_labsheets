79501f74cf879d21991ee1e07707674dc600ab16
SalithaEkanayaka123 <ekanayakasalitha1@gmail.com>
2020-11-18 17:59:55 +0530

    Notification labsheet over

--- app/src/main/java/com/sartainstudios/notificationtutorial/MainActivity.java
+++ app/src/main/java/com/sartainstudios/notificationtutorial/MainActivity.java
@@ -1,50 +1,64 @@
 package com.sartainstudios.notificationtutorial;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String CHANNEL_ID = "channel_1" ;
+    Button signup;
+    EditText username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Finds you button from the xml layout file
-        Button createNotificationButton = findViewById(R.id.button_create_notification);
+        signup = findViewById(R.id.signup);
+        username = findViewById(R.id.username);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "salitha";
+            String description ="SLIIT Undergraduate";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new
+                    NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
 
-        // Waits for you to click the button
-        createNotificationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Starts the function below
-                addNotification();
-            }
-        });
     }
 
     // Creates and displays a notification
-    private void addNotification() {
+    public void addNotification(View view) {
         // Builds your notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .setContentTitle("John's Android Studio Tutorials")
-                .setContentText("A video has just arrived!");
 
-        // Creates the intent needed to show the notification
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        builder.setContentIntent(contentIntent);
+        String notificationMessage = "Hello " + username.getText().toString() + " welcome to the MAD team";
 
-        // Add as notification
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(0, builder.build());
+        Intent intent = new Intent(MainActivity.this, secondActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0 , intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Notification")
+                .setContentText(notificationMessage)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(MainActivity.this);
+        notificationManagerCompat.notify(0,builder.build());
+        startActivity(intent);
     }
 }
