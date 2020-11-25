4d9ef8ca6b52b2a293bd16702cc747fb6c81bf21
SalithaEkanayaka123 <ekanayakasalitha1@gmail.com>
2020-11-25 08:10:37 +0530

    2019 june paper done

--- app/src/main/java/com/scorpion/pastpaperjune/AddPhotograph.java
+++ app/src/main/java/com/scorpion/pastpaperjune/AddPhotograph.java
@@ -52,7 +52,8 @@ public class AddPhotograph extends AppCompatActivity {
         photograph = new Photograph();
 
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapterCategory = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapterCategory = ArrayAdapter.createFromResource
+                (this, R.array.categories, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapterCategory.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
